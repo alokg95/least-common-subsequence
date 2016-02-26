5ef16da1b184971afba10dbb08ed0d8b16354888
Alok Gupta <alok.gupta@shiphawk.com>
2016-02-26 02:03:49 -0800

    bug fixes

--- LCS/main.cpp
+++ LCS/main.cpp
@@ -7,67 +7,105 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
 
 
-void lcs( char *X, char *Y, int m, int n )
+vector<string> input;
+string line;
+int line_count;
+string str1;
+string str2;
+int arg_count;
+int counter = 0;
+
+void find_all_LCS(int m, int n)
 {
     int L[m+1][n+1];
-    
-    /* Following steps build L[m+1][n+1] in bottom up fashion. Note
-     that L[i][j] contains length of LCS of X[0..i-1] and Y[0..j-1] */
-    for (int i=0; i<=m; i++)
-    {
+    for (int i=0; i<=m; i++) {
         for (int j=0; j<=n; j++)
         {
             if (i == 0 || j == 0)
                 L[i][j] = 0;
-            else if (X[i-1] == Y[j-1])
+            else if (str1[i-1] == str2[j-1])
                 L[i][j] = L[i-1][j-1] + 1;
             else
                 L[i][j] = max(L[i-1][j], L[i][j-1]);
         }
     }
     
-    // Following code is used to print LCS
+    
     int index = L[m][n];
+    int LCS_length = L[m][n];
     
-    // Create a character array to store the lcs string
     char lcs[index+1];
-    lcs[index] = '\0'; // Set the terminating character
-    
-    // Start from the right-most-bottom-most corner and
-    // one by one store characters in lcs[]
+    lcs[index] = '\0';
     int i = m, j = n;
     while (i > 0 && j > 0)
     {
-        // If current character in X[] and Y are same, then
-        // current character is part of LCS
-        if (X[i-1] == Y[j-1])
+        if (str1[i-1] == str2[j-1])
         {
-            lcs[index-1] = X[i-1]; // Put current character in result
-            i--; j--; index--;     // reduce values of i, j and index
+            lcs[index-1] = str1[i-1];
+            i--;
+            j--;
+            index--;
         }
-        
-        // If not same, then find the larger of two and
-        // go in the direction of larger value
         else if (L[i-1][j] > L[i][j-1])
             i--;
         else
             j--;
     }
     
-    // Print the lcs
-    cout << "LCS of " << X << " and " << Y << " is " << lcs;
+    cout << LCS_length << " " << lcs << endl;
 }
 
-
 int main(int argc, const char * argv[]) {
-    char X[] = "AGGTAB";
-    char Y[] = "GXTXAYB";
-    int m = strlen(X);
-    int n = strlen(Y);
-    lcs(X, Y, m, n);
+    
+    while(getline(cin,line)) {
+        line_count = 0;
+        string ss;
+        istringstream iss(line);
+        while(iss >> ss) {
+            line_count++;
+        }
+        
+        if (line_count > 2) {
+            continue;
+        }
+        
+        if(line_count == 1){
+            arg_count = stoi(ss);
+            continue;
+        }
+        
+        istringstream iss2(line);
+        iss2 >> str1 >> str2;
+        if (!iss2) {
+            iss2.clear();
+            continue;
+        }
+        
+        input.push_back(str1);
+        input.push_back(str2);
+        counter++;
+        if(counter == arg_count){
+            break;
+        }
+    }
+    
+    str1 = "";
+    str2 = "";
+    
+    for(int i = 0; i < input.size(); i += 2){
+        str1 = input[i];
+        str2 = input[i+1];
+        int str1_length = str1.length();
+        int str2_length = str2.length();
+        find_all_LCS(str1_length, str2_length);
+    }
 
     return 0;
 }
